1fe0f84572b4741470ee12cb0e73e54982cd8061
Hoyeon Moon <mhy991011@gmail.com>
2022-10-04 21:30:40 -0700

    first commit

---
+++ src/components/Movie.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom"
+
+function Movie({id, coverImg, title, summary, genres}){
+//movie component will receive all props from parent component
+    return  (
+      <div>
+        <img src={coverImg} alt={title}/>
+        <h2>
+          <Link to= {`/movie/${id}`}>{title}</Link>
+        </h2>
+        <p>{summary}</p>
+        <ul>
+        {genres.map((g) => (
+        <li key={g}>{g}</li>
+        ))}
+        </ul>
+      </div>
+      )
+}
+
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    coverImg : PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import Movie from "../components/Movie";
+import {useState, useEffect} from "react";
+
+function Home(){ //localhost:3000
+  const [loading, setLoading]= useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await(
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  },[]);
+  console.log(movies);
+    
+  return (
+  <div>
+    {loading ? (
+      <h1>Loading...</h1>
+    ):(
+      <div>
+        {movies.map((movie) => (
+          <Movie 
+          key={movie.id}
+          id={movie.id}
+          coverImg={movie.medium_cover_image} 
+          title={movie.title} 
+          summary={movie.summary} 
+          genres={movie.genres} /> //sending them as props to Movie component
+        ))}
+      </div>
+    )}
+  </div>
+  );
+}
+
+export default Home;
